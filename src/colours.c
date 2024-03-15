56897251169374b8127f7debc63bf9f9555e6fab
Liath Vliegers <livliege@f0r1s12.codam.nl>
2024-03-15 18:40:26 +0100

    tried to fix the colours, i think im almost there, but the lines are green, instead of red... BUT IM DONE FOR TODAY because its stupit. i need to make sure the string gets the right length before i try to convert it to a hex. oke bye see you tomorrow

--- src/colours.c
+++ src/colours.c
@@ -6,7 +6,7 @@
 /*   By: livliege <livliege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 18:46:50 by livliege          #+#    #+#             */
-/*   Updated: 2024/03/15 17:13:48 by livliege         ###   ########.fr       */
+/*   Updated: 2024/03/15 18:33:13 by livliege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,33 +41,84 @@ int get_a(int rgba)
     return (rgba & 0xFF);
 }
 
-uint32_t convert_colour(char* colour_str)
-{
-	(void)colour_str;
-	
-	// FF0000
-	return(0xFFFFFFFF);
-}
 
+// uint32_t convert_colour(char* colour_str)
+// {
+// 	(void)colour_str;
+	
+// 	// FF0000
+// 	return(0xFFFFFFFF);
+// }
+
+
+
+//////////////////////////////////////////
+// HOW DOES THIS WORK???????????????????????????????? FML
+//////////////////////////////////////////
+
+
+
+int	atohex(char *hex)
+{
+	int	i;
+	int	numb;
+
+	i = 0;
+	numb = 0;
+
+	while (hex[i] != '\0') 
+	{
+        if (hex[i] >= '0' && hex[i] <= '9')
+            numb = numb * 16 + (hex[i] - '0');
+        else if (hex[i] >= 'a' && hex[i] <= 'f')
+            numb = numb * 16 + (hex[i] - 'a' + 10);
+        else if (hex[i] >= 'A' && hex[i] <= 'F')
+            numb = numb * 16 + (hex[i] - 'A' + 10);
+        i++;
+    }
+	ft_printf("numb = %d\n", numb);
+
+    return (numb);
+}
 
 uint32_t get_colour(char* line)
 {
 	char		**split_line;
 	char		*colour;
 	char		*col;
-	uint32_t 	hex_colour;
+	int			r;
+	int			g;
+	int			b;
+	int			a;
+	int			int_colour;
+	uint32_t 	rgba;
 
 	split_line = ft_split(line, ',');
 	if (split_line == NULL)
 		ft_exit(2);	// 2: ERROR: Memory allocation failed
-	colour = ft_substr(split_line[1], 2, 6);
+	// colour = ft_substr(split_line[1], 2, 6);
+	colour = split_line[1];
+
+	ft_printf("coloursssss = %s\n", colour);
+
 	col = ft_strtrim(colour, "\n");
-	ft_printf("colour = %s", col);
-	hex_colour = convert_colour(col);
+	int_colour = atohex(col);
+
+	ft_printf("int_colour = %d\n", int_colour);
+	
+	r = get_r(int_colour);
+	g = get_g(int_colour);
+	b = get_b(int_colour);
+	a = 0xFF;
+	rgba = ft_pixel_colour(r, g, b, a);
 	free(col);
-	free(colour);
+	// free(colour);
 	ft_free_matrix(split_line);
-	return(hex_colour);
+	
+	ft_printf("colour = %X\n", rgba);
+	
+	return(rgba);
+	// return(0xFFFFFFFF);
 }
 
 
